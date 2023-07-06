b5f71132ac71b88c4e13ee7f0b6944e455d266fe
Juan Munoz <j.munoz@lacroix.group>
2023-07-06 15:52:17 -0600

    Repository pattern: Categorias container implementation done. Good up to lesson 43.

---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/ContendorTrabajo.cs
@@ -0,0 +1,35 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public class ContendorTrabajo : IContenedorTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContendorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Categoria = new CategoriaRepository(_db);
+                
+        }
+
+        public ICategoriaRepository Categoria { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        ICategoriaRepository Categoria { get; }
+        //Aqui se deben de ir agregando los diferentes repositorios
+
+        void Save();
+    }
+}
