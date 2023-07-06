ec70d1d4bbb1647a2370eb68a304fb39ed31b6af
Juan Munoz <j.munoz@lacroix.group>
2023-07-06 15:33:42 -0600

    Generic and Categorias repositories (patter) done. Good up to lesson 42.

---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface ICategoriaRepository :IRepository<Categoria>
+    {
+        IEnumerable<SelectListItem> GetListaCategorias();
+
+        void Update(Categoria categoria);
+
+    }
+}
