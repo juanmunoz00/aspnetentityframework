8fe5753dda749cbe17309f631c430011d9e557d0
Juan Munoz <j.munoz@lacroix.group>
2023-07-26 14:33:36 -0600

    Articulos form with category drop down ready. Site launches and works fine, just a warning after articulo is created. Data is strored correctly in DB and folder. Good up to lesson 58.

--- BlogCore/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
+++ BlogCore/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -3,6 +3,7 @@ using BlogCore.Data;
 using BlogCore.Models;
 using BlogCore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogCore.Areas.Admin.Controllers
 {
@@ -10,12 +11,15 @@ namespace BlogCore.Areas.Admin.Controllers
     public class ArticulosController : Controller
     {
         private readonly IContenedorTrabajo _contenedorTrabajo;
-        private readonly ApplicationDbContext _context;
+        //private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public ArticulosController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        //public ArticulosController(IContenedorTrabajo contenedorTrabajo, ApplicationDbContext context)
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
         {
             _contenedorTrabajo = contenedorTrabajo;
-            _context = context;
+            //_context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
@@ -36,6 +40,42 @@ namespace BlogCore.Areas.Admin.Controllers
             return View(artivm);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ArticuloVM artiVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                if ( artiVM.Articulo.Id == 0)
+                {
+                    // Nuevo artículo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+
+                    artiVM.Articulo.UrlImage = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    artiVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+
+                    _contenedorTrabajo.Articulo.Add(artiVM.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            artiVM.ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias();
+            return View(artiVM);
+
+        }
 
         #region Llamadas a la API
 
