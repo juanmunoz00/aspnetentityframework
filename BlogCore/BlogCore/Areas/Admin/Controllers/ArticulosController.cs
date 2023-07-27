5c45f1ab1e6540e51d4564e84ecb6772d04d46d9
Juan Munoz <j.munoz@lacroix.group>
2023-07-27 14:01:53 -0600

    Articulos update ready. Site launches and works fine, just a warning after articulo is created. Data is strored correctly in DB and folder. Good up to lesson 61.

--- BlogCore/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
+++ BlogCore/BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -77,6 +77,82 @@ namespace BlogCore.Areas.Admin.Controllers
 
         }
 
+        public IActionResult Edit(int? id)
+        {
+            ArticuloVM artivm = new ArticuloVM()
+            {
+                Articulo = new BlogCore.Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+            };
+
+            if (id != null)
+            {
+                artivm.Articulo = _contenedorTrabajo.Articulo.Get(id.GetValueOrDefault());
+            }
+
+            return View(artivm);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ArticuloVM artiVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string rutaPrincipal = _hostingEnvironment.WebRootPath;
+                var archivos = HttpContext.Request.Form.Files;
+
+                var articuloDesdeDb = _contenedorTrabajo.Articulo.Get(artiVM.Articulo.Id);
+
+                if (archivos.Count() > 0)
+                {
+                    // Nueva imagen para el articulo
+                    string nombreArchivo = Guid.NewGuid().ToString();
+                    var subidas = Path.Combine(rutaPrincipal, @"imagenes\articulos");
+                    var extension = Path.GetExtension(archivos[0].FileName);
+                    var nuevaExtension = Path.GetExtension(archivos[0].FileName);
+
+                    var rutaImagen = Path.Combine(rutaPrincipal, articuloDesdeDb.UrlImage.TrimStart('\\') );
+
+                    if(System.IO.File.Exists(rutaImagen))
+                    {
+                        System.IO.File.Delete(rutaImagen);
+                    }
+
+                    // Nuevamente subir el archivo
+                    using (var fileStreams = new FileStream(Path.Combine(subidas, nombreArchivo + extension), FileMode.Create))
+                    {
+                        archivos[0].CopyTo(fileStreams);
+                    }
+
+                    artiVM.Articulo.UrlImage = @"\imagenes\articulos\" + nombreArchivo + extension;
+                    artiVM.Articulo.FechaCreacion = DateTime.Now.ToString();
+
+                    _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                    _contenedorTrabajo.Save();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Cuando la imagen ya existe y se conserva
+                    artiVM.Articulo.UrlImage = articuloDesdeDb.UrlImage;
+
+                }
+
+                _contenedorTrabajo.Articulo.Update(artiVM.Articulo);
+                _contenedorTrabajo.Save();
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(artiVM);
+
+        }
+
         #region Llamadas a la API
 
         [HttpGet]
